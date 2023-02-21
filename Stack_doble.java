cc54e4cc753913d2da5568b37800c0a9c1cf6a67
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-02-20 19:58:11 -0600

    Update Stack_doble.java

--- Stack_doble.java
+++ Stack_doble.java
@@ -1,1 +1,32 @@
+/**
+ * @author sofia.velasquez,dsf
+ */
+import java.util.DoublyLinkedList;
 
+public class stack_<T> implements IStack<T>
+{ 
+    private DoublyLinkedList<T> lista;
+
+    public Stack() {
+        lista = new ArrayList<T>();
+    }
+
+    public int count() {
+        return lista.size();
+    }
+
+    public boolean isEmpty() {
+        return lista.isEmpty();
+    }
+    public void push(T e) {
+        lista.add(0, e);
+    }
+
+    public T pull() {
+        return lista.remove(0);
+    }
+
+    public T peek() {
+        return lista.get(0);
+    }
+}
