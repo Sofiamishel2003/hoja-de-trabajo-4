09c9b2457889a2005980f2d56cf47019e3db1c8f
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-02-20 19:56:08 -0600

    Update Stack_vector.java

--- Stack_vector.java
+++ Stack_vector.java
@@ -1,1 +1,34 @@
 
+/**
+ * @author sofia.velasquez,dsf
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class stack_<T> implements IStack<T>
+{ 
+    private Vector<T> vec;
+    
+    public Stack() {
+        vec = new Vector<T>();
+    }
+
+    public int count() {
+        return vec.size();
+    }
+
+    public boolean isEmpty() {
+        return vec.isEmpty();
+    }
+    public void push(T e) {
+        vec.add(0, e);
+    }
+
+    public T pull() {
+        return vec.remove(0);
+    }
+
+    public T peek() {
+        return vec.get(0);
+    }
+}
