eb18a7711e51f1b40694cc95eb23c2113dd04831
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-02-20 19:50:53 -0600

    Update Stack_ArrayList.java

--- Stack_ArrayList.java
+++ Stack_ArrayList.java
@@ -1,1 +1,34 @@
 
+/**
+ * @author sofia.velasquez,dsf
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class stack_<T> implements IStack<T>
+{ 
+    private ArrayList<T> lista;
+
+    public Stack() {
+        lista = new ArrayList<T>();
+    }
+
+    public int count() {
+        return lista.size();
+    }
+
+    public boolean isEmpty() {
+        return lista.isEmpty();
+    }
+    public void push(T e) {
+        lista.add(0, e);
+    }
+
+    public T pull() {
+        return lista.remove(0);
+    }
+
+    public T peek() {
+        return lista.get(0);
+    }
+}
