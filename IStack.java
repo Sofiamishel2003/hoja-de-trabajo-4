d4e55689c870e1e02a8d8d1ba9f6624433700f42
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-02-20 19:47:42 -0600

    Update IStack.java

--- IStack.java
+++ IStack.java
@@ -1,1 +1,33 @@
 
+//
+//
+//  @ Project : hoja de trabajo 4
+//  @ File Name : IStack.java
+//  @ Date : 31/01/2023
+//  @ Author : Sofía Velásquez, Joaquín Campos, Julio García Salas
+//  obtenido de la hoja de trabajo 2 en grupo de los autores (se reciclo codigo)
+//
+public interface IStack<T>{
+	
+	void push(T e);
+    /***
+	 * ingresa un valor al stack
+	 * @param e  el valor a ingresar
+	 */
+	T peek();
+	/***
+	 * @return retorna el que se encuentra en Top
+	 */
+	T pull();
+	/***
+	 * @return retorna el valor actual despúes del pop
+	 */
+	boolean isEmpty();
+	/***
+	 * @return retorna si está vacio o no (True-False)
+	 */
+    int count();
+	/***
+	 * @return retorna si está vacio o no (True-False)
+	 */
+}
