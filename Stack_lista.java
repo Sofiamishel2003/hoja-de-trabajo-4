af48565adcec5c7792824952f8aafdfbec727c64
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-02-20 20:00:53 -0600

    Update Stack_lista.java

--- Stack_lista.java
+++ Stack_lista.java
@@ -1,1 +1,32 @@
+/**
+ * @author sofia.velasquez,dsf
+ */
+import java.util.SinglyLinkedList;
 
+public class stack_<T> implements IStack<T>
+{ 
+    private SinglyLinkedList<T> lista;
+
+    public Stack() {
+        lista = new SinglyLinkedList<T>();
+    }
+
+    public int count() {
+        return lista.size();
+    }
+
+    public boolean isEmpty() {
+        return lista.isEmpty();
+    }
+    public void push(T e) {
+        lista.add(0, e);
+    }
+
+    public T pull() {
+        return lista.remove(0);
+    }
+
+    public T peek() {
+        return lista.get(0);
+    }
+}
